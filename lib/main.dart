053a80cd657a850b2869a185f276cd446a5d1e57
M.Rohail Naveed <69760472+mrohail-dev@users.noreply.github.com>
2021-09-30 00:00:49 +0500

    commit 1

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: Colors.red
+      ),
+      home: Scaffold(
+        appBar: AppBar(title:Center(child: Text("Login"))),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+             children: [
+               Container(
+                 height: 200,
+                 width: 400,
+                 color: Colors.red,
+               ),
+               SizedBox(height:20),
+               Container(
+                 height: 200,
+                 width: 400,
+                color: Colors.red,
+               ),
+                              SizedBox(height:20),
+               Container(
+                 height: 200,
+                 width: 400,
+                color: Colors.red,
+               ),
+                              SizedBox(height:20),
+
+               Container(
+                 height: 200,
+                 width: 400,
+                 color: Colors.red,
+               ),
+                              SizedBox(height:20),
+
+               Container(
+                 height: 200,
+                 width: 400,
+                color: Colors.red,
+               ),
+               ElevatedButton(onPressed: (){print("Login Button is Working");}, child: Text(("Login")))
+             ],
+            ),
+          ),
+        ),
+        
+      ),
+    );
+  }
+}
+
